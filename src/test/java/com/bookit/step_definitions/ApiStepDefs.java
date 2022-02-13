a30e7c7039a4a0651a469b099a58ac46ecfb70b6
Murodil <murodil@cybertekschool.com>
2022-02-13 14:22:56 -0500

    BookIt automation

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,17 +1,8 @@
 package com.bookit.step_definitions;
 
-import com.bookit.utilities.BookItApiUtil;
-import com.bookit.utilities.DBUtils;
-import com.bookit.utilities.Environment;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import io.restassured.http.ContentType;
-import io.restassured.path.json.JsonPath;
-import io.restassured.response.Response;
-import org.junit.Assert;
-
-import java.util.Map;
 
 import static io.restassured.RestAssured.*;
 
@@ -21,13 +12,18 @@ public class ApiStepDefs {
 
     }
 
-    @Given("User sends request \\/api\\/users\\/me")
-    public void user_sends_request_api_users_me() {
-        System.out.println("user_sends_request_api_users_me");
+    @Given("User sends GET request to {string}")
+    public void user_sends_GET_request_to(String path) {
+
     }
 
-    @Then("content type is application\\/json")
-    public void content_type_is_application_json() {
+    @Then("status code should be {int}")
+    public void status_code_should_be(int expStatusCode) {
+
+    }
+
+    @Then("content type is {string}")
+    public void content_type_is(String expContentType) {
 
     }
 }
--- src/test/resources/features/api/login.feature
+++ src/test/resources/features/api/login.feature
@@ -1,8 +1,8 @@
-@bookit
+
   Feature: BookIt API login verifications
 
     Scenario: verify login with valid credentials
       Given User logged in to Bookit api as teacher role
-      And User sends request /api/users/me
+      And User sends GET request to "/api/users/me"
       Then status code should be 200
-      And content type is application/json
+      And content type is "application/json"
