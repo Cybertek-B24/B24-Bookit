7b3d235d753eed549bbece2261ae0dd15a8c4228
Murodil <murodil@cybertekschool.com>
2022-02-13 14:38:13 -0500

    BookIt automation

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,20 +1,32 @@
 package com.bookit.step_definitions;
 
 
+import com.bookit.utilities.BookItApiUtil;
+import com.bookit.utilities.Environment;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 
 public class ApiStepDefs {
+
+    String accessToken;
+    Response response;
+
     @Given("User logged in to Bookit api as teacher role")
     public void user_logged_in_to_Bookit_api_as_teacher_role() {
-
+        accessToken = BookItApiUtil.getAccessToken(Environment.TEACHER_EMAIL, Environment.TEACHER_PASSWORD);
+        System.out.println("Teacher email = " + Environment.TEACHER_EMAIL);
+        System.out.println("Teacher password = " +  Environment.TEACHER_PASSWORD);
     }
 
     @Given("User sends GET request to {string}")
     public void user_sends_GET_request_to(String path) {
-
+        response = given().accept(ContentType.JSON)
+                .and().header("Authorization", accessToken)
+                .when().get(Environment.BASE_URL + path);
     }
 
     @Then("status code should be {int}")
